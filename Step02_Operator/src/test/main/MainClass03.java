69eab7ee24fdd504ba19f77b03f89ef7c7312c07
Choiyoojin <lcxyurzn@naver.com>
2023-08-12 13:58:15 +0900

    연산자

---
+++ Step02_Operator/src/test/main/MainClass03.java
@@ -0,0 +1,23 @@
+package test.main;
+/*
+ *  3. 비교 연산자
+ *     비교 연산의 결과는 boolean type 을 리턴해준다.
+ *  ==, !=, >, >=, <, <= 
+ */
+public class MainClass03 {
+	public static void main(String[] args) {
+		boolean result1 = 10 == 10; //true
+		boolean result2 = 10 != 10; //false
+		boolean result3 = 10 > 100; //false;
+		boolean result4 = 10 >= 10; //true
+		boolean result5 = 10 < 100; //true
+		boolean result6 = 10 <= 10; //true	
+		
+		// String type 변수 name 에 null 대입하기 
+		String name=null;
+		
+		// null 인지 아닌지 비교 가능 (java 에서 null 값은 비교 가능)
+		boolean result7 = name == null; //true
+		boolean result8 = name != null; //false		
+	}
+}
